88b5d71578244ddaaa7d28ee51f0d2be83b9508c
1934312258 <1934312258@qq.com>
2019-09-15 22:28:29 +0800

    java

---
+++ JavaDemo/src/main/java/com/kevin/demo/FilterDemo/FilterConfig.java
@@ -0,0 +1,30 @@
+package com.kevin.demo.FilterDemo;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.FilterRegistration;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author kevin
+ * @date 2019-9-15 18:48
+ * @description todo
+ **/
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<TimeFilter> timeFilter(){
+        FilterRegistrationBean<TimeFilter>registrationBean=new FilterRegistrationBean<>();
+        TimeFilter timeFilter=new TimeFilter();
+        registrationBean.setFilter(timeFilter);
+        List<String>urls=new ArrayList<>();
+        //urls.add("/user/*")
+        urls.add("/*");
+        registrationBean.setUrlPatterns(urls);
+        return  registrationBean;
+
+    }
+}
---
+++ first-util/src/main/java/com/example/firstutil/FirstUtilApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstUtilApplication.class, args);
+    }
+
+}
